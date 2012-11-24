4293db4c21aa9b0a1261f88c26f6842f37e35e4c
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-11-24 16:46:40 +0100

    Renamed and moved class file.

--- classes/jyggen/YoutubeThumb.php
+++ classes/jyggen/YoutubeThumb.php
@@ -1,5 +1,18 @@
 <?php
-class YoutubeThumbnail
+/**
+* A PHP class created to help you load, edit and save thumbnails from videos on Youtube.
+*
+* @package YoutubeThumb
+* @version 1.0
+* @author Jonas Stendahl
+* @license MIT License
+* @copyright 2012 Jonas Stendahl
+* @link http://github.com/jyggen/YoutubeThumb
+*/
+
+namespace jyggen;
+
+class YoutubeThumb
 {
 
     protected $data, $id;
